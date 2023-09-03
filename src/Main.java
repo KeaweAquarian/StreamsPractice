6a4197046292013bf08dc983fd44b17b316420b1
KeaweA <keaweaquarian@gmail.com>
2023-09-02 20:21:06 -0400

    starting

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        String[] arr = {"a", "c", "b"};
+        List<String> list = Arrays.asList(arr);
+
+        List<String> listSorted = list.stream().sorted().toList();
+        System.out.println(listSorted);
+    }
+}
